e50dd130f50d8a8b18df80ac1cc910a12ccb219c
07kundan <kundankumar28884@gmail.com>
2024-05-22 16:47:37 +0530

    first commit

---
+++ src/app/accord/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+function Accord() {
+  return (
+    <div className="flex items-center justify-center">
+      <Accordion className="w-1/2" type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
+
+export default Accord;
